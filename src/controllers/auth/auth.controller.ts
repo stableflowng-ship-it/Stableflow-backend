5ccc865ef1d3f76f2c85c608879fb8bb4233bbf1
PeluMike <pmichael3006@gmail.com>
2025-08-29 14:27:09 -0600

    refactor: improve error handling and add transaction routes

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -27,7 +27,7 @@ export class UserController {
         path: '/',
         httpOnly: true,
         secure: envHelper.environ !== "localhost",
-        sameSite: envHelper.environ === "localhost" ? 'lax' : 'none',
+        sameSite: envHelper.environ === "localhost" ? 'none' : 'none',
         domain: envHelper.environ !== "localhost" ? '' : undefined,
         maxAge: 60 * 60 * 24 * 14,
       }
