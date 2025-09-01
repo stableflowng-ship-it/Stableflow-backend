4e2c4788af133868f12fc04d2d0e973d3c8f76a5
PeluMike <pmichael3006@gmail.com>
2025-09-01 10:55:13 -0600

    more updates to webhook

--- src/services/wallet/wallet.service.ts
+++ src/services/wallet/wallet.service.ts
@@ -115,12 +115,12 @@ export class WalletService {
       wallet.amount = wallet.amount + parseFloat(payload.data.amount)
       await walletRepo.save(wallet)
 
-      // if (business.auto_offramp) {
-      //   const order = await this.createOrderPaycrest({ accountName: business.bankDetails.accountName, accountNumber: business.bankDetails.accountNumber, amount: parseFloat(payload.data.amount), bankName: business.bankDetails.bankName, network: 'base', returnAddress: wallet.wallet_address, token: 'USDC', reference: transaction.reference })
-      //   await this.withdrawBlockradar({ address: order['receiveAddress'], amount: parseFloat(payload.data.amount) }, user)
-      //   transaction.status = "PROCESSING"
-      //   await transRepo.save(transaction)
-      // }
+      if (business.auto_offramp) {
+        const order = await this.createOrderPaycrest({ accountName: business.bankDetails.accountName, accountNumber: business.bankDetails.accountNumber, amount: parseFloat(payload.data.amount), bankName: business.bankDetails.bankName, network: 'base', returnAddress: wallet.wallet_address, token: 'USDC', reference: transaction.reference })
+        await this.withdrawBlockradar({ address: order['receiveAddress'], amount: parseFloat(payload.data.amount) }, user)
+        transaction.status = "PROCESSING"
+        await transRepo.save(transaction)
+      }
     } else if (payload.event === "withdraw.success") {
     }
     else {
@@ -180,50 +180,40 @@ export class WalletService {
   static withdrawBlockradar = async (payload: WithdrawalType, user: User) => {
     const business = await busiRepo.createQueryBuilder('busi').where('busi.owner_id =:ownerId', { ownerId: user.id }).getOne()
     const wallet = await walletRepo.createQueryBuilder('wallet').where('wallet.business_id =:businessId', { businessId: business.id }).getOne()
+
+    const walletId = envHelper.block_radar.wallet_id
+    const headers = {
+      "x-api-key": apiKey,
+      "Content-Type": "application/json"
+    }
+    const asset = await fetch(`${b_baseUrl}/${walletId}/assets`, { headers: headers })
+    if (!asset.ok) {
+      throw new HttpException(400, asset.statusText)
+    }
+    const asset_res: any = await asset.json()
     const body = {
-      assetId: envHelper.block_radar.wallet_id,
-      address: payload.amount,
-      amount: payload.amount.toString(),
+      assets: [{
+        id: asset_res.data[0].id,
+        address: payload.address,
+        amount: payload.amount.toString(),
+      }]
     }
-    const response = await fetch(`${b_baseUrl}/${envHelper.block_radar.wallet_id}/withdraw`, {
-      method: "POST",
-      headers: {
-        "API-Key": envHelper.block_radar.api_key,
-        "Content-Type": "application/json"
-      },
+    const url = `${b_baseUrl}/${walletId}/withdraw`;
+    const options = {
+      method: 'POST',
+      headers: headers,
       body: JSON.stringify(body)
-    })
+    };
 
-    if (!response.ok) {
-      throw new HttpException(400, 'Something went wrong')
+
+    const response = await fetch(url, options);
+    const data: any = await response.json();
+
+    if (data.statusCode !== 200) {
+      throw new HttpException(400, data.message)
     }
-    const res = await response.json()
-    // const data: any = await response.json()
-    // if (!business.auto_offramp) {
-    //   const newTrans = transRepo.create({
-    //     transaction_id: data.data.id,
-    //     business_id: business.id,
-    //     token_amount: parseFloat(data.data.amount),
-    //     fiat_amount: 0,
-    //     fiat_currency: 'ngn',
-    //     token: data.data.asset.symbol,
-    //     chain: data.data.asset.standard || "base",
-    //     token_logo: data.data.asset.logoUrl,
-    //     gatewayTxId: data.data.hash,
-    //     metadata: data.data.metadata,
-    //     type: "WITHDRAWAL",
-    //     txHash: data.data.blockHash,
-    //     senderAddress: data.data.senderAddress,
-    //     businessAddress: wallet.wallet_address,
-    //     address_id: wallet.address_id,
-    //     wallet_id: wallet.id,
-    //     exchange_rate: 0,
-    //     business: business
-    //   })
-    //   await transRepo.save(newTrans)
-    // }
 
-    return { message: 'Withdrawal initiated', data: res }
+    return { message: 'Withdrawal request initiated initiated', data: data }
   }
 
   //webhook for paycrest
@@ -234,50 +224,27 @@ export class WalletService {
   }
 }
 
-
-
-
-
-
-
-const data = {
-  "address": "0x2DC6836e58697Bf4Afd9BbA4C2330E1032cc9618",
-  "name": "Mike_enterprises_ec3d1755-d942-446e-82dd-76992e581a8a",
-  "type": "INTERNAL",
-  "derivationPath": "m/44'/60'/0'/0/1",
-  "metadata": {
-    "business_id": "ec3d1755-d942-446e-82dd-76992e581a8a",
-    "user_id": "57e382ab-96d1-46be-b8cc-bae7bbe5cf9c",
-    "wallet_type": "bep20usdc"
-  },
-  "configurations": {
-    "aml": {
-      "provider": "ofac, fbi, tether, circle",
-      "status": "success",
-      "message": "Address is not sanctioned"
-    },
-    "showPrivateKey": false,
-    "disableAutoSweep": false,
-    "enableGaslessWithdraw": false
-  },
-  "network": "testnet",
-  "blockchain": {
-    "id": "28a730d3-211b-40f7-bb8f-dd589dcc738e",
-    "name": "base",
-    "symbol": "eth",
-    "slug": "base",
-    "derivationPath": "m/44'/60'/0'/0",
-    "isEvmCompatible": true,
-    "isL2": true,
-    "isActive": true,
-    "tokenStandard": null,
-    "createdAt": "2024-06-07T11:09:56.586Z",
-    "updatedAt": "2024-11-26T15:26:21.825Z",
-    "logoUrl": "https://res.cloudinary.com/blockradar/image/upload/v1716800080/crypto-assets/Base_Network_Logo_vqyh7r.png"
-  },
-  "key": null,
-  "id": "f05df715-8127-46b8-99ef-1c0928e4a6da",
-  "isActive": true,
-  "createdAt": "2025-09-01T00:22:19.706Z",
-  "updatedAt": "2025-09-01T00:22:19.706Z"
-}
+// const data: any = await response.json()
+// if (!business.auto_offramp) {
+//   const newTrans = transRepo.create({
+//     transaction_id: data.data.id,
+//     business_id: business.id,
+//     token_amount: parseFloat(data.data.amount),
+//     fiat_amount: 0,
+//     fiat_currency: 'ngn',
+//     token: data.data.asset.symbol,
+//     chain: data.data.asset.standard || "base",
+//     token_logo: data.data.asset.logoUrl,
+//     gatewayTxId: data.data.hash,
+//     metadata: data.data.metadata,
+//     type: "WITHDRAWAL",
+//     txHash: data.data.blockHash,
+//     senderAddress: data.data.senderAddress,
+//     businessAddress: wallet.wallet_address,
+//     address_id: wallet.address_id,
+//     wallet_id: wallet.id,
+//     exchange_rate: 0,
+//     business: business
+//   })
+//   await transRepo.save(newTrans)
+// }
